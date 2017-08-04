9955c0bc3bab3c943127504ac70abdfcd8b30dd2
Tony <tony@gmail.com>
2017-08-04 18:26:29 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/maxweather/db/City.java
@@ -0,0 +1,46 @@
+package com.example.maxweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by mouca on 2017/8/4.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int provinceId;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+}
